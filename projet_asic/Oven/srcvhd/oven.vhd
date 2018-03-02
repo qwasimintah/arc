980f62dd241a5bb961e5f40de393dfee2801420f
zbc <aasmovsdd@gmail.com>
2018-03-02 15:22:56 +0100

    oven

--- projet_asic/Oven/srcvhd/oven.vhd
+++ projet_asic/Oven/srcvhd/oven.vhd
@@ -12,6 +12,64 @@ architecture Controller of Oven_ctrl is
 type States is (Idle, Full_power_on, Half_power_on, Set_time, Operation_enabled, Operation_disabled, Operating, Complete);
 signal state,nextstate: States;
 begin
+    process(state, Half_power, Full_power, Start, s30, s60, s120,
+            Time_set, Door_open, Timeout) 
+    begin
+        case state is
+            when Idle =>
+                if Half_power = '1' then nextstate <= Half_power_on;
+                elsif Full_power = '1' then nextstate <= Full_power_on;
+                else nextstate <= state;
+                end if;
+            when Full_power_on =>
+                if Half_power = '1' then nextstate <= Half_power_on;
+                elsif s30 = '1' or s60 = '1' or s120 = '1' then
+                    nextstate <= Set_time;
+                else nextstate <= state;
+                end if;
+            when Half_power_on =>
+                if Full_power = '1' then nextstate <= Full_power_on;
+                elsif s30 = '1' or s60 = '1' or s120 = '1' then
+                    nextstate <= Set_time;
+                else nextstate <= state;
+                end if;
+            when Set_time =>
+                if Timeset = '1' and Door_open = '0' then nextstate <= Operation_enabled;
+                elsif Timeset = '1' and Door_open = '1' then nextstate <= Operation_disabled;
+                else nextstate <= state;
+                end if;
+            when Operation_enabled =>
+                if Start = '1' then nextstate <= Operating;
+                else nextstate <= state;
+                end if;
+            when Operation_disabled =>
+                if Door_open = '0' then nextstate <= Operation_enabled;
+                else nextstate <= state;
+                end if;
+            when Operating =>
+                if Timeout = '1' then nextstate <= Complete;
+                elsif Door_open = '1' then nextstate <= Operation_disabled; 
+                else nextstate <= state;
+                end if;
+            when Complete =>
+                if Door_open = '1' then nextstate <= Idle;
+                else nextstate <= state;
+                end if;
+        end case;
+    end process;
+
+
+	process(reset, clk)
+	begin
+		if (reset='1') then state<=Idle;
+		elsif (clk'event and clk='1') then
+			state <= nextstate;
+		end if;
+	end process;
+	
+    process(state, Half_power, Full_power, Start, s30, s60, s120,
+    begin
+    end process;
 
 end Controller;
 
